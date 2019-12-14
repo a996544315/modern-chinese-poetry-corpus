a666135f5e14b312d69ff89cdaa4e732767c75aa
xixixix <zhanghengyang1996@qq.com>
2019-12-14 19:42:38 +0800

    新增Windows编辑脚本

---
+++ data/origin/add.bat
@@ -0,0 +1,28 @@
+@echo off
+
+REM 请将该脚本移动到具体的诗人目录下再使用
+REM %1%=诗歌标题
+REM %2%=创作日期
+
+if "%1%"=="" (
+echo 请输入诗歌名称
+goto entrance
+) 
+
+if exist %fileName% (
+echo 文件已存在。如需覆盖，请先使用下述命令删除旧文件
+echo del %fileName%
+goto entrance
+)
+
+REM 使用UTF-8
+chcp 65001
+
+set fileName=%1%.pt
+set firstLine=title:%1%
+set nextLine=date:%2%
+echo %firstLine% > %fileName%
+echo %nextLine% >> %fileName%
+start %fileName%
+
+:entrance
